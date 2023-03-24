919417686b2ba643fb153942a48c15e4de369aa5
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-03-24 22:25:53 +0300

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="App">
       Hello World!
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
