2d5b01d146aed22b09e82a568c037c40809d3e58
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-03-26 20:30:30 +0300

    add emotion

---
+++ readmd.md
@@ -0,0 +1,9 @@
+# Стартовый шаблон
+
+Данный шаблон включает:
+1. React + TS
+2. eslint + prettier
+3. react-router
+
+dev версия запускается командой npm run dev
+Для тестов используется команда npm run test
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,13 +3,8 @@ import { Link } from 'react-router-dom';
 function NotFound() {
   return (
     <>
-      <h1 className="mb-5">Not Found</h1>
-      <Link
-        className="flex items-center justify-center box-border px-2 w-32 h-8 text-white bg-blue-500 hover:bg-blue-600"
-        to="/"
-      >
-        Go Home
-      </Link>
+      <h1>Not Found</h1>
+      <Link to="/">Go Home</Link>
     </>
   );
 }
